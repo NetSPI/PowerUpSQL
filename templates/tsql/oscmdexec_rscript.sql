96e5b9e5f8c92bf75fa3619fbf7b5d5b54e8afcd
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-07 12:19:51 -0500

    Update oscmdexec_rscript.sql

--- templates/tsql/oscmdexec_rscript.sql
+++ templates/tsql/oscmdexec_rscript.sql
@@ -1,17 +1,30 @@
--- Dependences: R runtime must be installed
+-- Requirement: R must be setup during the installation.
 
--- Enable Show Advanced Options
-sp_configure 'Show Advanced Options',1
-RECONFIGURE
-GO
+-- Enable advanced options
+sp_configure 'show advanced options',1
+reconfigure
+go
 
--- Enable external scripts enabled, may require a service restart
+-- Enable external scripts
+-- Requires a restart of the SQL Server service to take effect
+-- User must have "EXECUTE ANY EXTERNAL SCRIPT" privilege
 sp_configure 'external scripts enabled',1
-RECONFIGURE
-GO
+reconfigure WITH OVERRIDE
+go
 
 EXEC sp_execute_external_script
   @language=N'R',
   @script=N'OutputDataSet <- data.frame(system("cmd.exe /c dir",intern=T))'
   WITH RESULT SETS (([cmd_out] text));
 GO
+
+-- Disable external scripts
+-- Requires a restart of the SQL Server service to take effect
+sp_configure 'external scripts enabled',0
+reconfigure WITH OVERRIDE
+go
+
+-- Disable advanced options
+sp_configure 'show advanced options',0
+reconfigure
+go

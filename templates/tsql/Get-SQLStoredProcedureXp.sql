2807b34fca82f32776f61f2a406ab65fded4318f
Scott Sutherland <nullbind@users.noreply.github.com>
2017-09-15 11:18:27 -0500

    Update Get-SQLStoredProcedureXp.sql

--- templates/tsql/Get-SQLStoredProcedureXp.sql
+++ templates/tsql/Get-SQLStoredProcedureXp.sql
@@ -4,7 +4,29 @@
   Author: Scott Sutherland, 2017
 */
 
-SELECT * FROM sys.objects o 
+SELECT	o.object_id,
+		o.parent_object_id,
+		o.schema_id,
+		o.type,
+		o.type_desc,
+		o.name,
+		o.principal_id,
+		s.text,
+		s.ctext,
+		s.status,
+		o.create_date,
+		o.modify_date,
+		o.is_ms_shipped,
+		o.is_published,
+		o.is_schema_published,
+		s.colid,
+		s.compressed,
+		s.encrypted,
+		s.id,
+		s.language,
+		s.number,
+		s.texttype
+FROM sys.objects o 
 INNER JOIN sys.syscomments s
-ON o.object_id = s.id
+		ON o.object_id = s.id
 WHERE o.type = 'x' 

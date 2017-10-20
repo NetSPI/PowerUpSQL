a9841618b79c55d14a55b48c660b153b29181674
Scott Sutherland <scott.sutherland@nullbind.com>
2017-10-20 08:58:25 -0500

    Add  Get-SQLOleDbProvder placeholder
    Add  Get-SQLOleDbProvder placeholder

--- tests/PowerUpSQLTests.ps1
+++ tests/PowerUpSQLTests.ps1
@@ -13,6 +13,7 @@
 ######################################################
 
 <#
+Get-SQLOleDbProvder 
 Get-SQLInstanceDomain              
 Get-SQLInstanceFile                
 Get-SQLInstanceLocal               

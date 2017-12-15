ea64f82d952f43d9e11aebb7ff740cd58bbbe0ed
Scott Sutherland <nullbind@users.noreply.github.com>
2017-12-14 21:20:03 -0600

    Update Get-Database.sql

--- templates/tsql/Get-Database.sql
+++ templates/tsql/Get-Database.sql
@@ -4,21 +4,23 @@
 --	If the "VIEW ANY DATABASE" privilege has been revoked from Public
 --	then some databases may not be listed if the current user is not a sysadmin.
 -- Reference: https://msdn.microsoft.com/en-us/library/ms178534.aspx
--- fix is_encrypted column - should only show on newer versions
+-- TODO: Fix is_encrypted column - should only show on versions =>10
 
-SELECT a.database_id as [dbid],
-	a.name,
-	HAS_DBACCESS(a.name) as [has_dbaccess],
-	SUSER_SNAME(a.owner_sid) as [db_owner],
-	a.is_trustworthy_on,
-	a.is_db_chaining_on,
-	a.is_broker_enabled,
-	a.is_encrypted,
-	a.is_read_only,
-	a.create_date,
-	a.recovery_model_desc,
-	b.filename 
-FROM [sys].[databases] a
-INNER JOIN [sys].[sysdatabases] b
-	ON a.database_id = b.dbid
-ORDER BY a.database_id
+SELECT      	@@SERVERNAME as [Instance],
+            	a.database_id as [DatabaseId],
+            	a.name as [DatabaseName],
+            	SUSER_SNAME(a.owner_sid) as [DatabaseOwner],
+		IS_SRVROLEMEMBER('sysadmin',SUSER_SNAME(a.owner_sid)) as [OwnerIsSysadmin],
+		a.is_trustworthy_on,
+		a.is_db_chaining_on,
+		a.is_broker_enabled,
+           	a.is_encrypted,
+           	a.is_read_only,
+		a.create_date,
+            	a.recovery_model_desc,
+            	b.filename as [FileName],
+            	(SELECT CAST(SUM(size) * 8. / 1024 AS DECIMAL(8,2)) from sys.master_files where name like a.name) as [DbSizeMb],
+            	HAS_DBACCESS(a.name) as [has_dbaccess]
+FROM		[sys].[databases] a
+INNER JOIN	[sys].[sysdatabases] b 
+ON		a.database_id = b.dbid 

076a12cf33b563bcf2d335a40ca4e5f59465d518
Scott Sutherland <nullbind@users.noreply.github.com>
2018-02-04 22:47:59 -0600

    Update Get-SQLForcedEncryptionSetting.sql

--- templates/tsql/Get-SQLForcedEncryptionSetting.sql
+++ templates/tsql/Get-SQLForcedEncryptionSetting.sql
@@ -2,9 +2,14 @@
 -- Description: Get the "Forced Encryption" setting for the current SQL Server instance
 -- Author: Scott Sutherland, NetSPI 2018
 
-DECLARE @ForcedEncryption INT
-EXEC master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',
-N'SOFTWARE\MICROSOFT\Microsoft SQL Server\MSSQLServer\SuperSocketNetLib',
-N'ForceEncryption', @ForcedEncryption OUTPUT
+BEGIN TRY 
+	DECLARE @ForcedEncryption INT
+	EXEC master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',
+	N'SOFTWARE\MICROSOFT\Microsoft SQL Server\MSSQLServer\SuperSocketNetLib',
+	N'ForceEncryption', @ForcedEncryption OUTPUT
 
-SELECT @ForcedEncryption as ForcedEncryption
+	SELECT @ForcedEncryption as ForcedEncryption
+END TRY
+BEGIN CATCH
+	SELECT 'NA'
+END CATCH

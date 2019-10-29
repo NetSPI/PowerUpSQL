15727ba91064d42d25d16281a39dbf2217749ca5
Scott Sutherland <nullbind@users.noreply.github.com>
2019-10-28 22:35:26 -0500

    Create Get-GlobalTempTableData.sql

---
+++ templates/tsql/Get-GlobalTempTableData.sql
@@ -0,0 +1,32 @@
+-- Script: Get-GlobalTempTableData.sql
+-- Author: Scott Sutherland
+
+---------------------------------------
+-- View All Global Temp Table Contents
+---------------------------------------
+
+-- Setup variables
+DECLARE @mytempname varchar(max)
+DECLARE @psmyscript varchar(max)
+
+-- Iterate through all global temp tables 
+DECLARE MY_CURSOR CURSOR 
+	FOR SELECT name FROM tempdb.sys.tables WHERE name LIKE '##%'
+OPEN MY_CURSOR
+FETCH NEXT FROM MY_CURSOR INTO @mytempname 
+WHILE @@FETCH_STATUS = 0
+BEGIN 
+
+	-- Print table name
+    PRINT @mytempname 
+	
+	-- Select table contents
+	DECLARE @myname varchar(max)
+	SET @myname = 'SELECT * FROM [' + @mytempname + ']'
+	EXEC(@myname)
+	
+	-- Next 
+	FETCH NEXT FROM MY_CURSOR INTO @mytempname 
+END
+CLOSE MY_CURSOR
+DEALLOCATE MY_CURSOR

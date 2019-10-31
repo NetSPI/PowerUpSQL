7824f2ef87bf2f0587a88403309d199987259cb7
Scott Sutherland <nullbind@users.noreply.github.com>
2019-10-31 17:29:23 -0500

    Update Get-TempTableColumns.sql

--- templates/tsql/Get-TempTableColumns.sql
+++ templates/tsql/Get-TempTableColumns.sql
@@ -1,11 +1,12 @@
-
 -- List temp tables, columns, and column types
-SELECT (SELECT CASE WHEN (select len(t1.name) - len(replace(t1.name,'#',''))) > 1 THEN 1 ELSE 0 END) as GlobalTempTable, 
-	   (SELECT CASE WHEN t1.name like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as LocalTempTable,
-	   (SELECT CASE WHEN t1.name not like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as TableVariable,
-	   t1.name as 'tablename',
+SELECT t1.name as 'tablename',
 	   t2.name as 'columnname',
-	   t3.name 
+	   t3.name,
+	   t1.create_date,
+	   t1.modify_date,
+	   (SELECT CASE WHEN (select len(t1.name) - len(replace(t1.name,'#',''))) > 1 THEN 1 ELSE 0 END) as GlobalTempTable, 
+	   (SELECT CASE WHEN t1.name like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as LocalTempTable,
+	   (SELECT CASE WHEN t1.name not like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as TableVariable
 FROM tempdb.sys.objects AS t1
 JOIN tempdb.sys.columns AS t2 ON t1.OBJECT_ID = t2.OBJECT_ID
 JOIN sys.types AS t3 ON t2.system_type_id = t3.system_type_id

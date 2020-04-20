4ce1446cf7405cc6fa65d28412f7a3e43239c78d
Scott Sutherland <nullbind@users.noreply.github.com>
2020-04-19 21:14:04 -0500

    Update PowerUpSQL.ps1

--- PowerUpSQL.ps1
+++ PowerUpSQL.ps1
@@ -3,7 +3,7 @@
         File: PowerUpSQL.ps1
         Author: Scott Sutherland (@_nullbind), NetSPI - 2020
         Major Contributors: Antti Rantasaari and Eric Gruber
-        Version: 1.104.14
+        Version: 1.104.15
         Description: PowerUpSQL is a PowerShell toolkit for attacking SQL Server.
         License: BSD 3-Clause
         Required Dependencies: PowerShell v.2

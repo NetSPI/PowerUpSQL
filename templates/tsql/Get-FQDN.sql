d90b64cff0725a19d14c1295bc74da69c35951fb
Scott Sutherland <nullbind@users.noreply.github.com>
2022-07-08 21:37:58 -0500

    Update Get-FQDN.sql

--- templates/tsql/Get-FQDN.sql
+++ templates/tsql/Get-FQDN.sql
@@ -1,5 +1,12 @@
 -- Requires sysadmin
 
+-- option 1
 DECLARE @Domain NVARCHAR(100)
 EXEC master.dbo.xp_regread 'HKEY_LOCAL_MACHINE', 'SYSTEM\CurrentControlSet\services\Tcpip\Parameters', N'Domain',@Domain OUTPUT
+SELECT Cast(SERVERPROPERTY('MachineName') as nvarchar) + '.' + @Domain AS FQDN
+
+
+-- option 2
+DECLARE @Domain NVARCHAR(100)
+EXEC master.dbo.xp_regread 'HKEY_LOCAL_MACHINE', 'SYSTEM\ControlSet001\Control\Lsa\CachedMachineNames', N'NameUserPrincipal',@Domain OUTPUT
 SELECT Cast(SERVERPROPERTY('MachineName') as nvarchar) + '.' + @Domain AS FQDN

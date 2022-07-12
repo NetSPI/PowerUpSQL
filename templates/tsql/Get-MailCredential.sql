f9513cef5cb0d86a0562e448227d3088c1d1fd28
Scott Sutherland <nullbind@users.noreply.github.com>
2022-07-11 21:34:21 -0500

    Update Get-MailCredential.sql

--- templates/tsql/Get-MailCredential.sql
+++ templates/tsql/Get-MailCredential.sql
@@ -1,7 +1,7 @@
 -- Script: Get-MailCredential.sql
 -- Requirements: Sysadmin or required SELECT privileges.
 -- Description: Returns a row for SMTP credential.  Everything but the cleartext credential is shown.
--- Note: Tested on SQL Server 2008, 2014, 2016.
+-- Note: Tested on SQL Server 2008, 2012, 2014, 2016.
 
 SELECT c.name as credential_name,  
 c.credential_id,

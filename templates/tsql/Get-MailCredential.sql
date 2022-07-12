f42cc4ee476199a4abfdbdd6b10766861a828ab7
Scott Sutherland <nullbind@users.noreply.github.com>
2022-07-11 21:23:07 -0500

    Update Get-MailCredential.sql

--- templates/tsql/Get-MailCredential.sql
+++ templates/tsql/Get-MailCredential.sql
@@ -3,13 +3,18 @@
 -- Description: Returns a row for SMTP credential.  Everything but the cleartext credential is shown.
 -- Reference: https://msdn.microsoft.com/en-us/library/ms187404.aspx
 
-SELECT name,  
+SELECT c.name as credential_name,  
 c.credential_id,
 ms.account_id,
 ms.servertype,
 ms.servername,
 ms.port,
 ms.username,
+a.name,
+a.display_name,
+a.description,
+a.email_address,
+a.replyto_address,
 ms.credential_id,
 ms.use_default_credentials,
 ms.enable_ssl,
@@ -19,5 +24,7 @@ ms.last_mod_datetime,
 ms.last_mod_user
 FROM sys.credentials as c       
 JOIN msdb.dbo.sysmail_server as ms        
-ON c.credential_id = ms.credential_id     
+ON c.credential_id = ms.credential_id   
+JOIN msdb.dbo.sysmail_account as a
+ON ms.account_id = a.account_id
 WHERE ms.servertype like 'SMTP'

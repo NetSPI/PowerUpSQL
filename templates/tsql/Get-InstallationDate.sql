aaa76000b30076a77ca67a3792ece36ffa971b1a
Scott Sutherland <nullbind@users.noreply.github.com>
2022-12-22 15:48:46 -0600

    Update Get-InstallationDate.sql

--- templates/tsql/Get-InstallationDate.sql
+++ templates/tsql/Get-InstallationDate.sql
@@ -1,4 +1,5 @@
 -- requires sysadmin
+-- works on 2022, 2016, but not on 2014
 -- https://www.dbrnd.com/2016/03/sql-server-script-to-find-installation-date-time-and-authentication-mode/
 SELECT 
 	createdate AS InstallationDate 

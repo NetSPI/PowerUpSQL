46c0448ee4d42d20b0171a8d71eaccb76cef8a25
Scott Sutherland <nullbind@users.noreply.github.com>
2022-12-22 15:00:58 -0600

    Create Get-InstallationDate.sql

---
+++ templates/tsql/Get-InstallationDate.sql
@@ -0,0 +1,11 @@
+-- requires sysadmin
+-- https://www.dbrnd.com/2016/03/sql-server-script-to-find-installation-date-time-and-authentication-mode/
+SELECT 
+	createdate AS InstallationDate 
+	,CASE SERVERPROPERTY('IsIntegratedSecurityOnly')   
+		WHEN 1 THEN 'Windows Authentication'  
+		WHEN 0 THEN 'Windows and SQL Server Authentication'  
+	END AS AuthenticationMode
+	,SERVERPROPERTY('servername') AS ServerName
+FROM master.sys.syslogins 
+WHERE name LIKE 'NT AUTHORITY\SYSTEM'

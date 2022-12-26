54d73c340611a00e1a2c683c96e7057f7dc35e49
Scott Sutherland <nullbind@users.noreply.github.com>
2022-12-26 16:59:48 -0600

    Update Get-InstallationDate.sql

--- templates/tsql/Get-InstallationDate.sql
+++ templates/tsql/Get-InstallationDate.sql
@@ -1,6 +1,5 @@
 -- Option 1: createdat FROM master.sys.syslogins 
--- $server.VersionMajor -ge 9
--- Tested version: 2022, 2016, 2014
+-- Tested version: 2022, 2016, 2014, 2012
 -- Requirements: sysadmin
 -- Reference: https://www.dbrnd.com/2016/03/sql-server-script-to-find-installation-date-time-and-authentication-mode/
 SELECT 
@@ -13,8 +12,11 @@ SELECT
 FROM master.sys.syslogins 
 WHERE name LIKE 'NT AUTHORITY\SYSTEM'
 
+
 -- Option 2: create_date FROM sys.server_principals
 -- $server.VersionMajor -ge 9
+-- Tested version: 2022, 2016, 2014, 2012
+-- Requirements: sysadmin not required
 -- Reference: https://github.com/dataplat/dbatools/blob/6cae0dd18bda3ad8efd60404c2d05b402cc4a785/functions/Get-DbaInstanceInstallDate.ps1
 /*
 $sql = "SELECT create_date FROM sys.server_principals WHERE sid = 0x010100000000000512000000"
@@ -22,7 +24,9 @@ $sql = "SELECT create_date FROM sys.server_principals WHERE sid = 0x010100000000
 */
 
 -- Option 3: schemadate FROM sysservers
---$server.VersionMajor -le 9
+-- $server.VersionMajor -le 9
+-- Tested version: 2022, 2016, 2014, 2012
+-- Requirements: sysadmin not required
 --Reference: https://github.com/dataplat/dbatools/blob/6cae0dd18bda3ad8efd60404c2d05b402cc4a785/functions/Get-DbaInstanceInstallDate.ps1
 /*
 $sql = "SELECT schemadate FROM sysservers"

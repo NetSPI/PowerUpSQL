4d827b9f0e21aa2fadacb6f430b1f81881722802
Scott Sutherland <nullbind@users.noreply.github.com>
2024-05-16 08:06:18 -0500

    Update Get-TempTableColumns.sql
    Updated query to condense data structure.

--- templates/tsql/Get-TempTableColumns.sql
+++ templates/tsql/Get-TempTableColumns.sql
@@ -1,15 +1,19 @@
--- List temp tables, columns, and column types
-SELECT t1.name as 'Table_Name',
-	   t2.name as 'Column_Name',
-	   t3.name as 'Column_Type',
-	   t1.create_date,
-	   t1.modify_date,
-	   t1.parent_object_id,
-	   OBJECT_ID(t1.parent_object_id) as parent_object,
-	   (SELECT CASE WHEN (select len(t1.name) - len(replace(t1.name,'#',''))) > 1 THEN 1 ELSE 0 END) as GlobalTempTable, 
-	   (SELECT CASE WHEN t1.name like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as LocalTempTable,
-	   (SELECT CASE WHEN t1.name not like '%[_]%' AND (select len(t1.name) - len(replace(t1.name,'#',''))) = 1 THEN 1 ELSE 0 END) as TableVariable
+SELECT 
+    t1.name AS 'Table_Name',
+    t2.name AS 'Column_Name',
+    t3.name AS 'Column_Type',
+    CASE
+        WHEN (SELECT CASE WHEN LEN(t1.name) - LEN(REPLACE(t1.name,'#','')) > 1 THEN 1 ELSE 0 END) = 1 THEN 'GlobalTempTable'
+        WHEN t1.name LIKE '%[_]%' AND (SELECT CASE WHEN LEN(t1.name) - LEN(REPLACE(t1.name,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'LocalTempTable'
+        WHEN t1.name NOT LIKE '%[_]%' AND (SELECT CASE WHEN LEN(t1.name) - LEN(REPLACE(t1.name,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'TableVariable'
+        ELSE NULL
+    END AS Table_Type,
+    t1.is_ms_shipped,
+    t1.is_published,
+    t1.is_schema_published,
+    t1.create_date,
+    t1.modify_date
 FROM tempdb.sys.objects AS t1
 JOIN tempdb.sys.columns AS t2 ON t1.OBJECT_ID = t2.OBJECT_ID
 JOIN sys.types AS t3 ON t2.system_type_id = t3.system_type_id
-WHERE t1.name like '#%';
+WHERE t1.name LIKE '#%';

8d4cb04fb62d4b814445496c064f6d52d58ff1fd
Scott Sutherland <nullbind@users.noreply.github.com>
2024-05-16 08:33:21 -0500

    Update Get-TempTableColumns.sql
    Added database name and schema name.

--- templates/tsql/Get-TempTableColumns.sql
+++ templates/tsql/Get-TempTableColumns.sql
@@ -3,7 +3,8 @@
 -- Description: Return a list of all temp table types.
 -- Include table variables, local temp tables, and global temp tables.
 
-SELECT 
+SELECT 'tempdb' as 'Database_Name',
+    SCHEMA_NAME(t1.schema_id) AS 'Schema_Name',
     t1.name AS 'Table_Name',
     t2.name AS 'Column_Name',
     t3.name AS 'Column_Type',
@@ -18,7 +19,7 @@ SELECT
     t1.is_schema_published,
     t1.create_date,
     t1.modify_date
-FROM tempdb.sys.objects AS t1
-JOIN tempdb.sys.columns AS t2 ON t1.OBJECT_ID = t2.OBJECT_ID
+FROM [tempdb].[sys].[objects] AS t1
+JOIN [tempdb].[sys].[columns] AS t2 ON t1.OBJECT_ID = t2.OBJECT_ID
 JOIN sys.types AS t3 ON t2.system_type_id = t3.system_type_id
-WHERE t1.name LIKE '#%';
+WHERE t1.name LIKE '#%'

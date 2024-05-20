d2d30581a30315d13b1900e9df342b7df96d8738
Scott Sutherland <nullbind@users.noreply.github.com>
2024-05-20 10:19:26 -0500

    Update Get-Table.sql

--- templates/tsql/Get-Table.sql
+++ templates/tsql/Get-Table.sql
@@ -2,13 +2,25 @@
 -- Description: Returns a list of tables for the current database.
 -- Reference: https://msdn.microsoft.com/en-us/library/ms186224.aspx
 
-SELECT TABLE_CATALOG AS [DATABASE_NAME],
-	TABLE_SCHEMA AS [SCHEMA_NAME],
-	TABLE_NAME,
-	CASE
-		WHEN (SELECT CASE WHEN LEN(TABLE_NAME) - LEN(REPLACE(TABLE_NAME,'#','')) > 1 THEN 1 ELSE 0 END) = 1 THEN 'GlobalTempTable'
-                WHEN TABLE_NAME LIKE '%[_]%' AND (SELECT CASE WHEN LEN(TABLE_NAME) - LEN(REPLACE(TABLE_NAME,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'LocalTempTable'
-                WHEN TABLE_NAME NOT LIKE '%[_]%' AND (SELECT CASE WHEN LEN(TABLE_NAME) - LEN(REPLACE(TABLE_NAME,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'TableVariable'
-                ELSE  TABLE_TYPE
-        END AS Table_Type
-FROM [INFORMATION_SCHEMA].[TABLES]
+SELECT 
+    @@SERVERNAME AS [INSTANCE_NAME],
+    t.TABLE_CATALOG AS [DATABASE_NAME],
+    t.TABLE_SCHEMA AS [SCHEMA_NAME],
+    t.TABLE_NAME,
+    CASE
+        WHEN (SELECT CASE WHEN LEN(t.TABLE_NAME) - LEN(REPLACE(t.TABLE_NAME,'#','')) > 1 THEN 1 ELSE 0 END) = 1 THEN 'GlobalTempTable'
+        WHEN t.TABLE_NAME LIKE '%[_]%' AND (SELECT CASE WHEN LEN(t.TABLE_NAME) - LEN(REPLACE(t.TABLE_NAME,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'LocalTempTable'
+        WHEN t.TABLE_NAME NOT LIKE '%[_]%' AND (SELECT CASE WHEN LEN(t.TABLE_NAME) - LEN(REPLACE(t.TABLE_NAME,'#','')) = 1 THEN 1 ELSE 0 END) = 1 THEN 'TableVariable'
+        ELSE t.TABLE_TYPE
+    END AS Table_Type,
+    st.is_ms_shipped,
+    st.is_published,
+    st.is_schema_published,
+    st.create_date,
+    st.modify_date AS modified_date
+FROM [INFORMATION_SCHEMA].[TABLES] t
+JOIN sys.tables st ON t.TABLE_NAME = st.name AND t.TABLE_SCHEMA = OBJECT_SCHEMA_NAME(st.object_id)
+JOIN sys.objects s ON st.object_id = s.object_id
+LEFT JOIN sys.extended_properties ep ON s.object_id = ep.major_id 
+    AND ep.minor_id = 0 
+ORDER BY t.TABLE_CATALOG, t.TABLE_SCHEMA, t.TABLE_NAME;
